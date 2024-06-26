c6f51f53a39152e86fc0acc840b41b641b2ec11e
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-06-26 13:20:34 +0100

    hi

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,6 @@
 const currentyear = document.querySelector("#currentyear");
 const today = new Date();
-currentyear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+currentyear.innerHTML = `${today.getFullYear()}`;
+const lastModified = document.querySelector("#lastmodified");
+const date = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modified: <span>${new Intl.DateTimeFormat("en-US",).format(date)}</span>`;
