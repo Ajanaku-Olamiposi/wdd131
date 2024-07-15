411bdf59481264b1ba0db0b8d498891010366c79
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-07-16 00:35:01 +0100

    hi

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -17,6 +17,7 @@ const temples = [
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
+    year: 2005,
     area: 11500,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -25,6 +26,7 @@ const temples = [
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
+    year: 1888,
     area: 74792,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -33,6 +35,7 @@ const temples = [
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
+    year: 2015,
     area: 96630,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -41,6 +44,7 @@ const temples = [
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
+    year: 2020,
     area: 6861,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -49,6 +53,7 @@ const temples = [
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
+    year: 1974,
     area: 156558,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -57,6 +62,7 @@ const temples = [
     templeName: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
+    year: 1986,
     area: 9600,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -65,6 +71,7 @@ const temples = [
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
+    year: 1983,
     area: 116642,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
@@ -73,31 +80,56 @@ const temples = [
     templeName: "Barranquilla Colombia",
     location: "Barranquilla Colombia",
     dedicated: "2018, December, 9",
+    year: 2018,
     area: 25349,
     imageUrl:
   "https://churchofjesuschristtemples.org/assets/img/temples/barranquilla-colombia-temple/barranquilla-colombia-temple-1846-main.jpg"
-  }
+  },
   {
     templeName: "Accra Ghana",
     location: "Accra Ghana",
     dedicated: "1998, February, 16",
+    year:1998,
     area: 17500,
     imageUrl:
     "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-13760-main.jpg"
-  }
+  },
   {
     templeName: "London England",
     location: "London England",
     dedicated: "1958, September, 9",
+    year:1958,
     area: 42652,
     imageUrl:
     "https://churchofjesuschristtemples.org/assets/img/temples/london-england-temple/london-england-temple-4243-main.jpg"
   }
-  // Add more temple objects here...
 ];
-createTempleCard();
-function createTempleCard(){
-  temples.forEach(
+
+createTempleCard(temples);
+
+const oldLink = document.querySelector("#old");
+oldLink.addEventListener("click",()=>{
+  createTempleCard(temples.filter(temple => temple.year < 1900));
+});
+
+const newLink = document.querySelector("#new");
+newLink.addEventListener("click",()=>{
+  createTempleCard(temples.filter(temple => temple.year > 2000));
+});
+
+const largeLink = document.querySelector("#large");
+largeLink.addEventListener("click",()=>{
+  createTempleCard(temples.filter(temple => temple.area > 90000));
+});
+
+const smallLink = document.querySelector("#small");
+smallLink.addEventListener("click",()=>{
+  createTempleCard(temples.filter(temple => temple.area < 10000));
+});
+
+function createTempleCard(filteredTemples){
+  document.querySelector(".magic").innerHTML = "";
+  filteredTemples.forEach(
     temple => {
       let card = document.createElement("section");
       let name = document.createElement("h3");
